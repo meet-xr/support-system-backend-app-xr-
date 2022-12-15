a5c20a5e92d831531932949135ae26d10e756acc
meet ribadiya <115990341+meet-xr@users.noreply.github.com>
2022-12-15 18:33:53 +0530

    create authorization api

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const PORT = 5000;
+
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log("Database connection Success.");
+    })
+    .catch((err) => {
+        console.error("Mongo Connection Error", err);
+    });
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); //optional
+
+app.use("/users", require("./routes/users"));
+app.listen(PORT, () => {
+    console.log("Server started listening on PORT : " + PORT);
+});
